6c603c0731f92f104b6cc50eda3daa524f769245
Alvin Francisco Prado Peynado <alvinprado185@gmail.com>
2024-11-28 04:32:10 -0600

    commit 3
    La verdad medio le entendi

--- main.dart
+++ main.dart
@@ -11,54 +11,58 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Proyecto Taller',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
-        textTheme: GoogleFonts.latoTextTheme(),
+        textTheme: GoogleFonts.anaheimTextTheme(),
       ),
       home: Scaffold(
         body: Row(
           children: [
             Column(
+              //mainaxis alinea verticalmente
+              // mainAxisAlignment: MainAxisAlignment.center,
+              //crossaxis alinea horizontalmente
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                customButton(Icons.dashboard, 'Dashboard', () {}),
-                customButton(Icons.assignment, 'Tasks', () {}),
-                customButton(Icons.share, 'Shared Items', () {}),
+                botton(Icons.book, 'Projects', () {}),
+                botton(Icons.draw, 'Draft', () {}),
+                botton(Icons.switch_access_shortcut_outlined, 'Shared whit me',
+                    () {}),
                 const Spacer(),
-                customButton(Icons.settings, 'Preferences', () {}),
-                customButton(Icons.group_add, 'Team Members', () {}),
-                customButton(Icons.create_new_folder, 'Create Task', () {}),
-                customButton(Icons.folder, 'Create Project', () {}),
+                botton(Icons.settings, 'Settings', () {}),
+                botton(Icons.people, 'Invite members', () {}),
+                botton(Icons.add, 'New Draft', () {}),
+                botton(Icons.add, 'New Project', () {}),
               ],
             ),
             Padding(
-              padding: const EdgeInsets.all(20.0),
+              padding: const EdgeInsets.all(30.0),
               child: Column(
                 children: [
                   const Row(
                     mainAxisSize: MainAxisSize.min,
                     children: [
                       Text(
-                        'My Chapulineada',
+                        'Side Hustle',
                         style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
+                          fontSize: 35,
                         ),
                       ),
                       SizedBox(width: 20),
                       Icon(
                         Icons.keyboard_arrow_down_rounded,
-                        size: 30,
+                        size: 35,
                       ),
-                      SizedBox(width: 800),
+                      SizedBox(width: 750),
                       Icon(Icons.link),
                       SizedBox(width: 20),
                       Text('Share'),
                       SizedBox(width: 20),
                       Icon(Icons.more_vert),
+                      //
                     ],
                   ),
                   Row(
@@ -66,11 +70,11 @@ class MainApp extends StatelessWidget {
                     mainAxisSize: MainAxisSize.max,
                     children: [
                       nota(),
-                      const SizedBox(width: 25),
+                      const SizedBox(width: 40),
                       nota(),
-                      const SizedBox(width: 25),
+                      const SizedBox(width: 40),
                       nota(),
-                      const SizedBox(width: 25),
+                      const SizedBox(width: 40),
                       nota(),
                     ],
                   ),
@@ -83,33 +87,88 @@ class MainApp extends StatelessWidget {
     );
   }
 
-  Widget nota() {
+  nota() {
     return Container(
       decoration: BoxDecoration(
-        color: Colors.blue[50]!,
+        color: Colors.blue[300],
         borderRadius: BorderRadius.circular(15),
       ),
-      padding: const EdgeInsets.all(20),
+      padding: const EdgeInsets.all(25),
       height: 300,
       width: 200,
       child: Column(
+        // crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
+            // mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Container(
-                height: 16,
-                width: 18,
-                decoration: BoxDecoration(
-                  color: Colors.blue[300]!,
-                  borderRadius: BorderRadius.circular(5),
+                height: 10,
+                width: 10,
+                decoration: const BoxDecoration(
+                  color: Colors.blue,
+                  shape: BoxShape.circle,
                 ),
               ),
-              const SizedBox(width: 10),
-              const Text(
-                'Padrino',
-                style: TextStyle(fontWeight: FontWeight.bold),
+              const SizedBox(width: 15),
+              const Text('Laguna del Chapulin Colorado'),
+            ],
+          ),
+          const SizedBox(height: 25),
+          const Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Text(
+                    'Este proyecto busca mejorar la infraestructura local.'),
+              ),
+            ),
+          ),
+          const SizedBox(height: 35),
+          Row(
+            children: [
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStateProperty.all(Colors.grey[100]),
+                    shape: WidgetStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  child: Text(
+                    'Ver detalles',
+                    style: TextStyle(
+                      color: Colors.blue[800],
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
-          const SizedBox(height: 20),
-   
+        ],
+      ),
+    );
+  }
+
+  InkWell botton(IconData icon, String text, Function onPressed) {
+    return InkWell(
+      onTap: () {
+        onPressed();
+        print(text);
+      },
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        child: Row(
+          children: [
+            Icon(icon),
+            const SizedBox(width: 20),
+            Text(text),
+          ],
+        ),
+      ),
+    );
+  }
+}
